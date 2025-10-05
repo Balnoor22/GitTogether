c3eb69c895042df17b719d35fc467796f28d58dd
Balnoor22 <noorroby22@gmail.com>
2025-10-05 14:57:09 +0530

    built user card on feed

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,7 +1,36 @@
-const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import { useEffect } from "react";
+import UserCard from "./UserCard";
 
-export default Feed
+const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+  const getFeed = async () => {
+    if (feed) return; //if feed data is already there in redux store,dont make API call
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res?.data?.data));
+    } catch (err) {
+      //Handle error
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return (
+    feed && ( //If feed is present then only load this page
+      <div className="flex justify-center my-10">
+        <UserCard user={feed[0]}/>
+      </div>
+    )
+  );
+};
+
+export default Feed;
---
+++ src/Components/UserCard.jsx
@@ -0,0 +1,26 @@
+const UserCard = ({ user }) => {
+  // console.log(user);
+  const { firstName, lastName, photoUrl, age, gender, about } = user;
+  return (
+    <div className="card bg-base-300 w-78 shadow-sm">
+      <figure className="flex justify-center items-center">
+        <img
+          src={user.photoUrl}
+          alt="Photo"
+          className="w-full max-h-60 object-cover rounded-t-lg"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age && gender && <p>{age + ", "+gender}</p>} {/*If age n gender are present then only show them*/}
+        <p>{about}</p>
+        <div className="card-actions flex justify-between my-3">
+          <button className="btn btn-primary">Ignore</button>
+          <button className="btn btn-secondary">Interested</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
