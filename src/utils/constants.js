62a2be0175179b5de14010d4715e1e5e483f4076
Balnoor22 <noorroby22@gmail.com>
2025-10-18 15:34:37 +0530

    made BASE_URL dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "/api";
+//if  running on localhost use first url and if not (production) then use "/api"
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
